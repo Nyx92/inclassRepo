8c4f67c0819eb68f054a847c262dae88869ce2ab
Nyx92 <ngyx92@gmail.com>
2022-01-29 11:21:36 +0800

    edited index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 let name = process.argv[2];
 console.log(`hello ${name.toUpperCase()}! How are you?`);
+
+import { writeFile } from 'fs';
+let name = process.argv[2];
+writeFile('data.json', name, (err) => console.error('err ->', err));
+
+console.log(`good day ${name}, are you well?`);
